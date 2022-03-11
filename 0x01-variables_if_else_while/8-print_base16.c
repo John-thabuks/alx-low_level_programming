5945db315a0760b62b54e8d5c9f61c88d86bfa40
John-thabuks <thabuksjohn@gmail.com>
2022-03-11 23:07:30 +0300

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - print a hexadecimal
+ *Return:0
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x);
+	}
+	for (x = 'a'; x <= 'f'; x++)
+	{
+		putchar(x)
+	}
+
+	return (0);
+}
