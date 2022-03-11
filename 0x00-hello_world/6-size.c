604b7cd8102ba10421a1e1122968877a73da20bb
John-thabuks <thabuksjohn@gmail.com>
2022-03-11 14:21:19 +0300

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ *main - Entry point
+ *
+ *Return: should be a 0
+ */
+int main(void)
+{
+printf("size of a char: %d bytes(s)\n", sizeof(char));
+printf("size of an int: %d byte(s)\n", sizeof(int));
+printf("size of a long int: %d bytes(s)\n", sizeof(long int));
+printf("size of a long long int: %d bytes(s)\n" sizeof(long long int));
+printf("size of a float: %f bytes(s)\n", sizeof(float));
+return (0);
+}
