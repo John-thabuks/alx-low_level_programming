b25ca2b32c9acb41ccc4fd96c8417ca1342acc9c
John-thabuks <thabuksjohn@gmail.com>
2022-03-11 13:30:05 +0300

    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,8 @@
  *Return: 0 after printing the function
  */
 
-int main (void){
-	put("Programming is like building a multilingual puzzle");
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
 	return(0);
 }
