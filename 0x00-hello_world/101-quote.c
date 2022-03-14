cb31aa4a9328f134afcb82a69867dbf3353ec775
John-thabuks <thabuksjohn@gmail.com>
2022-03-14 15:58:00 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	fwrite(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	fprintf(stdout, "%s", "and that piece of art is useful\"";
+	fprintf(stdout, "%s", " - Dora Korpar, 2015-10-19\n");
+	
 	return (1);
 }
