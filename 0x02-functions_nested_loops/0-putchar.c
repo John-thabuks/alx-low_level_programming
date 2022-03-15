f50b7879b2f0c3b43deb391def842de57c0fd02e
John-thabuks <thabuksjohn@gmail.com>
2022-03-15 21:28:56 +0300

    Task 0: correction on Betty spacing

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,15 @@
  **/
 int main(void)
 {
-        char thabuks[] = "_putchar";
+	char thabuks[] = "_putchar";
 
-        int x;
+	int x;
 
-        for (x = 0; x < 8; x++)
-        {
-                _putchar(thabuks[x]);
-        }
-        _putchar('\n');
-        return (0);
+	for (x = 0; x < 8; x++)
+	{
+		_putchar(thabuks[x]);
+	}
+	_putchar('\n');
+
+return (0);
 }
