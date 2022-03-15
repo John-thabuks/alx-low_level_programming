01c8bcbfb4386d6601e3f39f86426805ecfb7010
John-thabuks <thabuksjohn@gmail.com>
2022-03-15 22:37:30 +0300

    Task 1

---
+++ 0x02-functions_nested_loops/1-aphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * printAlphabet - prints alphabet in lowercase
+ *
+ **/
+void printAlphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+	_putchar('\n');
+}
