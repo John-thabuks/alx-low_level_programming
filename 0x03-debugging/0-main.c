357067c454375f62af6d15eb11de7ea658777831
John-thabuks <thabuksjohn@gmail.com>
2022-03-17 13:17:20 +0300

    Task 0: correction

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- *main - main
- *Return:0
+ * main - check the code
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-int i;
-i = 0;
+	int i;
 
-positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-return (0);
-
+	return (0);
 }
