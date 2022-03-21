69381cc3279e4157d8058c204897aab51f3779f7
John-thabuks <thabuksjohn@gmail.com>
2022-03-21 19:58:00 +0300

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - retrieves pointer int and updates
+ *               the value and points to 98.
+ * @n: The pointer to an int.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
